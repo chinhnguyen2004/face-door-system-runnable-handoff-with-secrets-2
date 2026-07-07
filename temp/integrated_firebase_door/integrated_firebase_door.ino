ddfdb01cf5f306e224d282b62e7baea8e8b73c5c
chinhnguyen2004 <chinhnguyen2004@users.noreply.github.com>
2026-07-07 15:06:48 +0700

    Fix integer overflow by using double data type for Firebase Unix timestamp parsing on ESP8266

--- temp/integrated_firebase_door/integrated_firebase_door.ino
+++ temp/integrated_firebase_door/integrated_firebase_door.ino
@@ -80,7 +80,7 @@ unsigned long captureStartedMs = 0;
 unsigned long doorOpenedMs = 0;
 unsigned long lastSensorMs = 0;
 unsigned long lastFirebaseMs = 0;
-int lastResultTimestamp = -1;
+double lastResultTimestamp = -1.0;
 float lastDistance = -1.0;
 
 void lcdLine(uint8_t row, String text) {
@@ -145,10 +145,10 @@ void setupFirebase() {
   Firebase.RTDB.setBool(&fbdo, capturePath.c_str(), false);
   
   // Đọc timestamp hiện tại để tránh nhận diện nhầm kết quả cũ khi vừa khởi động
-  if (Firebase.RTDB.getInt(&fbdo, resultTimePath.c_str())) {
-    lastResultTimestamp = fbdo.intData();
+  if (Firebase.RTDB.getDouble(&fbdo, resultTimePath.c_str())) {
+    lastResultTimestamp = fbdo.doubleData();
     Serial.print("Initial result timestamp: ");
-    Serial.println(lastResultTimestamp);
+    Serial.println(lastResultTimestamp, 0);
   }
 }
 
@@ -228,8 +228,8 @@ void requestCaptureIfNeeded() {
 }
 
 void checkRecognitionResult() {
-  if (Firebase.RTDB.getInt(&fbdo, resultTimePath.c_str())) {
-    int ts = fbdo.intData();
+  if (Firebase.RTDB.getDouble(&fbdo, resultTimePath.c_str())) {
+    double ts = fbdo.doubleData();
     if (ts != lastResultTimestamp) {
       lastResultTimestamp = ts;
       if (Firebase.RTDB.getString(&fbdo, resultPath.c_str())) {
